bc54ef3934e6b1c0137efb9f9c25965da631b039
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-04 16:04:16 -0500

    created database tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
 from .db import db
 from .user import User
+from .photo import Photo
+from .postcard import Postcard
+from .like import Like
+from .follower import Follower
---
+++ app/models/photo.py
@@ -0,0 +1,21 @@
+from .db import db
+from datetime import datetime
+
+class Photo(db.Model):
+	__tablename__ = 'photos'
+
+	id = db.Column(db.Integer, primary_key = True)
+	photo_url = db.Column(db.String, nullable=False, unique=True)
+	public = db.Column(db.Boolean, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+	created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+
+	def to_dict(self):
+		return {
+			'id': self.id,
+			'photo_url': self.photo_url,
+			'user_id': self.user_id,
+			'public': self.public,
+			'created_at': self.created_at,
+		}
