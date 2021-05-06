0247a3ccc74a4409f9764aa90cdc5feaaba46202
Chuck Kainrath <chuckkainrath@gmailcom>
2021-05-06 16:30:35 -0500

    added redirect to user profile on image click

--- react-app/src/components/MainPage/PhotosComponent/index.js
+++ react-app/src/components/MainPage/PhotosComponent/index.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { getPhotos } from '../../../store/photos';
 import styles from './PhotosComponent.module.css';
 
 function PhotosComponent() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const user = useSelector(state => state.session.user);
     const photos = useSelector(state => state.photos.photos);
     const photosArr = photos ? Object.values(photos) : [];
@@ -21,9 +22,9 @@ function PhotosComponent() {
                 photosArr.map(photo => {
                     return <img key={photo.id}
                                 className={styles.main_photo}
-                                src={photo.photo_url}>
-                                {/* onClick={<Redirect to={`/profiles/`}} */}
-                            </img>
+                                src={photo.photo_url}
+                                onClick={() => history.push(`/profiles/${photo.username}`)}
+                            />
                 })
             }
             {!photosArr && <h1>No photos yet</h1>}
