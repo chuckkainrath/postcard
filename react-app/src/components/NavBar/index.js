df22457e3aeaf0ad44d4848bc8591ae5ff1b09c1
Chuck Kainrath <chuckkainrath@gmailcom>
2021-05-06 18:39:05 -0500

    did general navbar outline

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,29 +1,51 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { logout } from '../../store/session';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
   const user = useSelector(state => state.session.user);
+  let profileSrc;
+  if (user) {
+    profileSrc = user.profile_img_url ? user.profile_img_url : '/images/blank-profile-img.webp';
+  }
+  const [profileMenu, showProfileMenu] = useState(false);
+
+  const onLogout = async () => {
+    await dispatch(logout());
+    history.push('/');
+  }
 
   return (
     <nav>
         {user &&
           <ul className={styles.navbar__container}>
             <li>
-              <NavLink to="/photos" exact={true} activeClassName="active">
+              <NavLink
+                to="/photos"
+                exact={true}
+                activeClassName="active"
+                className={styles.navbar__main}
+              >
                 Main Page
               </NavLink>
             </li>
             <li>
-              <NavLink to={`/profiles/${user.username}`}>
-                <i class="fas fa-user"></i>
-              </NavLink>
+                <img
+                  className={styles.profile__icon}
+                  src={profileSrc}
+                  onClick={() => showProfileMenu(!profileMenu)}
+                />
             </li>
-            <li>
-              <LogoutButton />
-            </li>
+            {profileMenu &&
+              <div className={styles.profile__dropdown}>
+                  <li onClick={() => history.push(`/profiles/${user.username}`)}>My Profile</li>
+                  <li onClick={onLogout}>Logout</li>
+              </div>
+            }
           </ul>
         }
         {!user &&
