d1d14f728b86b21dc946d103b9c45fc326310ba4
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-06 15:49:01 -0500

    displaying current users photos on profile page

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -5,11 +5,11 @@ from app.models import User, Photo
 user_routes = Blueprint('users', __name__)
 
 
-@user_routes.route('/<int:username>')
+@user_routes.route('/<username>')
 @login_required
 def user(username):
     user = User.query.filter(User.username == username).first()
-    sessionUser = int(current_user)
+    sessionUser = int(current_user.id)
     # Get all photos if request comes from session user
     # Otherwise, just public photos
     if sessionUser == user.id:
@@ -17,5 +17,5 @@ def user(username):
     else:
         photos = Photo.query.filter(Photo.user_id == user.id and
                                     Photo.public == True).all()
-    photos_dict = [photo.to_dict for photo in photos]
+    photos_dict = [photo.to_dict() for photo in photos]
     return { 'user': user.to_dict(), 'photos': photos_dict }
