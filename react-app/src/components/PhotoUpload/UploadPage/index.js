7fc029993d9aedf7bdb2eb78751f5be1e47f93e7
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-07 15:01:13 -0500

    added component to get redirected upon icon click

---
+++ react-app/src/components/PhotoUpload/UploadPage/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function UploadPage() {
+
+    return (
+        <h1>Upload Page</h1>
+    );
+}
+
+export default UploadPage;
