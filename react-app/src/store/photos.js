a5dc1e770eb6ccdaa8661eb3c47c550e38b706a3
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-13 15:15:51 -0500

    finsihed front end route for uploading photo and saving to redux store

--- react-app/src/store/photos.js
+++ react-app/src/store/photos.js
@@ -8,9 +8,28 @@ const getPhotosAction = photos => ({
     payload: photos
 })
 
+const postPhotoAction = photo => ({
+    type: POST_PHOTO,
+    action: photo
+})
+
 export const postPhoto = (photo, pvtPhoto) => async dispatch => {
     const form = new FormData();
-    // form.photo =
+    const picType = photo.type;
+    const ext = picType.split('/')[1];
+    const picFile = new File([photo], `newphoto.${ext}`);
+    form.append('photo', picFile);
+    form.append('private_photo', pvtPhoto);
+    const response = await fetch('/api/photos/', {
+        method: 'POST',
+        body: form
+    });
+    const data = await response.json();
+    if (data.errors) {
+        console.log('PHOTO UPLOAD ERRER', data.errors);
+        return;
+    }
+    dispatch(postPhotoAction(data.photo))
 }
 
 export const getPhotos = () => async dispatch => {
@@ -35,13 +54,19 @@ export const flattenPhotos = photos => {
 const initialState = { photos: null }
 
 export default function reducer(state = initialState, action) {
+    let newState;
     switch (action.type) {
         case GET_PHOTOS:
             return { photos: action.payload }
         case GET_PHOTO:
             return initialState; // TODO: COMPELTE ROUTE
         case POST_PHOTO:
-            return initialState; // TODO: COMPELTE ROUTE
+            newState = Object.assign({}, state);
+            if (!newState.photos) {
+                newState.photos = {}
+            }
+            newState.photos[action.payload.id] = action.payload
+            return newState;
         case DELETE_PHOTO:
             return initialState; // TODO: COMPELTE ROUTE
         default:
