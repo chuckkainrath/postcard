cf3118bc4eb70bbbd592c878fb1c10a2d14406de
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-20 17:11:50 -0500

    worked on deleting photo, having issues with aws

--- react-app/src/components/MainPage/PhotoCard/index.js
+++ react-app/src/components/MainPage/PhotoCard/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
+import { confirmAlert } from 'react-confirm-alert';
+import { deletePhoto } from '../../../store/photos';
 import styles from './PhotoCard.module.css';
 import blankProfile from './blank-profile-img.png';
 
-function PhotoCard({ photo }) {
+function PhotoCard({ profilePage, photo }) {
     const history = useHistory();
+    const dispatch = useDispatch();
     const profileSrc = photo.profile_img_url ? photo.profile_img_url : blankProfile;
-    console.log(profileSrc);
+
+    const dltPhoto = async () => {
+        dispatch(deletePhoto(photo.id));
+    }
+
+    const confirmDelete = () => {
+        const options = {
+            title: 'Delete Photo',
+            message: 'Are you sure you want to delete this photo?',
+            buttons: [
+                {
+                    label: 'Delete',
+                    onClick: dltPhoto
+                },
+                {
+                    label: 'Cancel',
+                    onClick: () => {}
+                }
+            ],
+            closeOnEscape: true,
+            closeOnClickOutside: true,
+            willUnmount: () => {},
+            afterClose: () => {},
+            onClickOutside: () => {},
+            onKeypressEscape: () => {},
+            overlayClassName: styles.delete_popup
+        };
+        confirmAlert(options);
+    }
+
     return (
         <div className={styles.photo__card}>
             <img
@@ -15,10 +48,15 @@ function PhotoCard({ photo }) {
                 onClick={() => history.push(`/profiles/${photo.username}`)}
             />
             <div className={styles.photo__options}>
-                <img
-                    className={styles.photo__user_profile}
-                    src={profileSrc}
-                />
+                {!profilePage &&
+                    <img
+                        className={styles.photo__user_profile}
+                        src={profileSrc}
+                    />
+                }
+                {profilePage &&
+                    <button onClick={confirmDelete}>Delete</button>
+                }
                 <div
                     onClick={() => history.push(`/create-postcard/${photo.id}`)}
                     className={styles.photo__card_create}>
