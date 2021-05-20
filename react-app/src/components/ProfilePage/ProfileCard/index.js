e298f1205e316296bb04586794c1c4812dbbeed9
Chuck Kainrath <chuckkainrath@gmailcom>
2021-05-20 17:15:53 -0500

    removed profile content out of photocard

---
+++ react-app/src/components/ProfilePage/ProfileCard/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { confirmAlert } from 'react-confirm-alert';
+import { deletePhoto } from '../../../store/photos';
+import styles from './PhotoCard.module.css';
+import blankProfile from './blank-profile-img.png';
+
+function ProfileCard({ photo }) {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const profileSrc = photo.profile_img_url ? photo.profile_img_url : blankProfile;
+
+    const dltPhoto = async () => {
+        dispatch(deletePhoto(photo.id));
+    }
+
+    const confirmDelete = () => {
+        const options = {
+            title: 'Delete Photo',
+            message: 'Are you sure you want to delete this photo?',
+            buttons: [
+                {
+                    label: 'Delete',
+                    onClick: dltPhoto
+                },
+                {
+                    label: 'Cancel',
+                    onClick: () => {}
+                }
+            ],
+            closeOnEscape: true,
+            closeOnClickOutside: true,
+            willUnmount: () => {},
+            afterClose: () => {},
+            onClickOutside: () => {},
+            onKeypressEscape: () => {},
+            overlayClassName: styles.delete_popup
+        };
+        confirmAlert(options);
+    }
+
+    return (
+        <div className={styles.photo__card}>
+            <img
+                className={styles.photo__img}
+                src={photo.photo_url}
+                onClick={() => history.push(`/profiles/${photo.username}`)}
+            />
+            <div className={styles.photo__options}>
+                {!profilePage &&
+                    <img
+                        className={styles.photo__user_profile}
+                        src={profileSrc}
+                    />
+                }
+                {profilePage &&
+                    <button onClick={confirmDelete}>Delete</button>
+                }
+                <div
+                    onClick={() => history.push(`/create-postcard/${photo.id}`)}
+                    className={styles.photo__card_create}>
+                    <i class="fal fa-envelope"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileCard;
