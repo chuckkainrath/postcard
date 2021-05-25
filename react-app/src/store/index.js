b80776da39fc8d0dc4935272c0b9ed093ffb6953
Chuck Kainrath <chuckkainrath@gmail.com>
2021-05-25 16:00:14 -0500

    added follows store and api routes

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -6,12 +6,14 @@ import session from './session';
 import photos from './photos';
 import profile from './profile';
 import postcards from './postcards';
+import followers from './followers';
 
 const rootReducer = combineReducers({
     session,
     photos,
     profile,
-    postcards
+    postcards,
+    followers
 })
 
 const persistConfig = {
