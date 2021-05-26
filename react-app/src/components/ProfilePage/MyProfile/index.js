d00ea5510f0ebcd6019f96c356ee50a3b0ae4e87
Chuck Kainrath <chuckkainrath@gmailcom>
2021-05-25 23:10:52 -0500

    added follow list to profile page

--- react-app/src/components/ProfilePage/MyProfile/index.js
+++ react-app/src/components/ProfilePage/MyProfile/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import styles from './MyProfile.module.css';
 import ProfileCard from '../ProfileCard';
 import PhotoCard from '../../MainPage/PhotoCard';
 import PostcardCard from '../PostcardCard';
 import { getPostcards } from '../../../store/postcards';
 import { getLikedPhotos } from '../../../store/photos';
+import { deleteFollow } from '../../../store/followers';
 
 const photoFilter = (photos, photoType) => {
     return photos.filter(photo => {
@@ -29,12 +31,25 @@ const postcardFilter = (postcards) => {
     return fPostcards;
 }
 
+const followSort = (follows) => {
+    if (!follows) return []
+    const sortedFollows = Object.values(follows);
+    sortedFollows.sort((follow1, follow2) => {
+        return follow1.username.toLowerCase() - follow2.username.toLowerCase();
+    })
+    return sortedFollows;
+}
+
 function MyProfile() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const profile = useSelector(state => state.profile);
     const photos = useSelector(state => state.photos);
     const postcardsDict = useSelector(state => state.postcards);
+    const { followers, following } = useSelector(state => state.follows);
     const [category, setCategory] = useState('photo-public');
+    const [followersArr, setFollowersArr] = useState([]);
+    const [followingArr, setFollowingArr] = useState([]);
     const [likedPhotos, setLikedPhotos] = useState(
         photos.likedPhotos ? Object.values(photos.likedPhotos).reverse() : []
     );
@@ -52,6 +67,14 @@ function MyProfile() {
         dispatch(getPostcards());
         dispatch(getLikedPhotos());
     }, []);
+
+    useEffect(() => {
+        setFollowingArr(followSort(following));
+    }, [following])
+
+    useEffect(() => {
+        setFollowersArr(followSort(followers));
+    }, [followers])
 
     useEffect(() => {
         setPostcards(postcardsDict ? postcardFilter(Object.values(postcardsDict.postcards)) : {})
@@ -78,6 +101,13 @@ function MyProfile() {
         selectedNode.classList.add(styles.category_selection__selected);
     }
 
+    const unfollow = async follow => {
+        await dispatch(deleteFollow(follow.id, follow.followed_id));
+        const followCopy = Object.assign({}, following);
+        delete followCopy[follow.id];
+        setFollowingArr(followSort(followCopy));
+    }
+
     return (
         <div className={styles.profile__container}>
             <h1>My Profile</h1>
@@ -98,34 +128,57 @@ function MyProfile() {
                     className={styles.category_selection__right}
                     onClick={(e) => setPictureCategory('postcards')}>Postcards</span>
             </div>
-            {category === 'photo-public' &&
-                <div className={styles.photos__container}>
-                    {publicPhotos && publicPhotos.map(photo => (
-                        <ProfileCard userProfile={true} key={photo.id} photo={photo} />
-                    ))}
+            <div className={styles.center__container}>
+                {category === 'photo-public' &&
+                    <div className={styles.photos__container}>
+                        {publicPhotos && publicPhotos.map(photo => (
+                            <ProfileCard userProfile={true} key={photo.id} photo={photo} />
+                        ))}
+                    </div>
+                }
+                {category === 'photo-private' &&
+                    <div className={styles.photos__container}>
+                        {privatePhotos && privatePhotos.map(photo => (
+                            <ProfileCard userProfile={true} key={photo.id} photo={photo} />
+                        ))}
+                    </div>
+                }
+                {category === 'liked-photos' &&
+                    <div className={styles.photos__container}>
+                        {likedPhotos && likedPhotos.map(photo => (
+                            <PhotoCard userProfile={false} key={photo.id} photo={photo} />
+                        ))}
+                    </div>
+                }
+                {category === 'postcards' &&
+                    <div className={styles.photos__container}>
+                        {postcards && Object.keys(postcards).map((key, idx) => (
+                            <PostcardCard cards={postcards[key]} key={idx} />
+                        ))}
+                    </div>
+                }
+            </div>
+            <div className={styles.left__container}>
+                <h1>Following</h1>
+                <div className={styles.follow__container}>
+                    {followingArr && followingArr.map(follow => {
+                        return (
+                            <div>
+                                <p onClick={() => history.push(`/profiles/${follow.username}`)}key={follow.id}>{follow.username}</p>
+                                <button onClick={() => unfollow(follow)}>Unfollow</button>
+                            </div>
+                        )
+                    })}
                 </div>
-            }
-            {category === 'photo-private' &&
-                <div className={styles.photos__container}>
-                    {privatePhotos && privatePhotos.map(photo => (
-                        <ProfileCard userProfile={true} key={photo.id} photo={photo} />
-                    ))}
+            </div>
+            <div className={styles.right__container}>
+                <h1>Followers</h1>
+                <div className={styles.follow__container}>
+                    {followersArr && followersArr.map(follow => {
+                        return <p key={follow.id}>{follow.username}</p>
+                    })}
                 </div>
-            }
-            {category === 'liked-photos' &&
-                <div className={styles.photos__container}>
-                    {likedPhotos && likedPhotos.map(photo => (
-                        <PhotoCard userProfile={false} key={photo.id} photo={photo} />
-                    ))}
-                </div>
-            }
-            {category === 'postcards' &&
-                <div className={styles.photos__container}>
-                    {postcards && Object.keys(postcards).map((key, idx) => (
-                        <PostcardCard cards={postcards[key]} key={idx} />
-                    ))}
-                </div>
-            }
+            </div>
         </div>
     );
 }
