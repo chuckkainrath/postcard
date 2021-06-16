fc29a812d650d9fe78d1d8243d8694247e1cd770
Chuck Kainrath <chuckkainrath@gmail.com>
2021-06-16 15:45:07 -0500

    removed unused login/signup code and added sign up link

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory, NavLink } from 'react-router-dom';
+import { useHistory, NavLink, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import LoginModal from '../auth/LoginModal';
@@ -14,24 +14,16 @@ function Splash() {
         history.push('/photos');
     }
 
-    const login = () => {
-        history.push('/login');
-    }
-
-    const signup = () => {
-        history.push('/sign-up');
-    }
-
     return (
         <div className={styles.page__container}>
             <div className={styles.splash__container}>
                 <h1 className={styles.splash__message}>Welcome to Postacard where you create postcards from photos!</h1>
                 <h1 className={styles.login_signup__container}>
-                    <Button onClick={() => setShowLogin(true)} className={styles.login__link}>
+                    <Button onClick={() => setShowLogin(true)}>
                         Login
                     </Button>
                     <span> or </span>
-                    <Button className={styles.signup__link}>
+                    <Button onClick={() => history.push('/sign-up')}>
                         Sign Up
                     </Button>
                 </h1>
