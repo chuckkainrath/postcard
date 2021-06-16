d5da721a4261fbacfd6b30f40e72e95e8cb809af
Chuck Kainrath <chuckkainrath@gmailcom>
2021-06-16 11:20:40 -0500

    refactored login signup out of Navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,7 +11,7 @@ import { authenticate } from "./store/session";
 import ProfileContainer from "./components/ProfilePage/ProfileContainer";
 import UploadPage from './components/PhotoUpload/UploadPage';
 import PostCard from './components/PostCard';
-import './bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
