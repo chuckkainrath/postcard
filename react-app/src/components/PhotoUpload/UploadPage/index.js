b8660021a54f4322dec4b065b6f038771f70385b
Chuck Kainrath <chuckkainrath@gmailcom>
2021-06-23 11:10:05 -0500

    added choose different photo and cancel options to photo upload

--- react-app/src/components/PhotoUpload/UploadPage/index.js
+++ react-app/src/components/PhotoUpload/UploadPage/index.js
@@ -1,7 +1,8 @@
 import React, { useCallback, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useDropzone } from 'react-dropzone';
+import { Modal, Button } from 'react-bootstrap';
 import AvatarEditor from 'react-avatar-editor';
 import { postPhoto } from '../../../store/photos';
 import styles from './UploadPage.module.css';
@@ -9,7 +10,7 @@ import styles from './UploadPage.module.css';
 const WIDTH = 600;
 const HEIGHT = 400;
 
-function UploadPage() {
+function UploadPage({uploadImage, showUploadImage}) {
     const dispatch = useDispatch();
     const history = useHistory();
     const [imageUrl, setImageUrl] = useState();
@@ -26,7 +27,6 @@ function UploadPage() {
             setImageUrl(URL.createObjectURL(imageFile));
         }
     }, []);
-
     const { getRootProps, getInputProps, isDragActive } = useDropzone({onDrop});
 
     const uploadPhoto = () => {
@@ -40,51 +40,65 @@ function UploadPage() {
         }
     }
 
+    const chooseDiff = () => {
+        setImageUrl('');
+    }
+
     return (
-        <div className={styles.upload__container}>
-            <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                { isDragActive ?
-                    <p>Drop the file here...</p> :
-                    <p>Drag 'n' drop a file here, or click to select files</p>
+        <Modal
+            show={uploadImage}
+            onHide={() => showUploadImage(false)}
+            centered
+        >
+            <div>
+                {!imageUrl &&
+                    <div className={styles.image__select} {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        { isDragActive ?
+                            <p className={styles.upload__msg}>Drop the file here...</p> :
+                            <p className={styles.upload__msg}>Drag 'n' drop a file here, <br /> or click to select files</p>
+                        }
+                    </div>
+                }
+                {imageUrl &&
+                    <div className={styles.image__crop}>
+                        <AvatarEditor
+                            image={imageUrl}
+                            width={WIDTH}
+                            height={HEIGHT}
+                            border={[10,10]}
+                            borderRadius={0}
+                            color={[255,255,255,0.6]}
+                            scale={scale}
+                            rotate={0}
+                            ref={(e) => setEditor(e)}
+                        />
+                        <div>
+                            <label>Scale</label>
+                            <input
+                                type='range'
+                                step='0.1'
+                                min='1'
+                                max='3'
+                                value={scale}
+                                onChange={(e) => setScale(parseFloat(e.target.value))}
+                            />
+                        </div>
+                        <div>
+                            <label>Make Photo Private</label>
+                            <input
+                                type='checkbox'
+                                checked={privatePhoto}
+                                onChange={() => setPrivatePhoto(!privatePhoto)}
+                            />
+                        </div>
+                        <Button disabled={imageUploaded} onClick={uploadPhoto}>Upload Photo</Button>
+                        <Button onClick={chooseDiff}>Choose a Different Photo</Button>
+                        <Button onClick={() => showUploadImage(false)}>Cancel</Button>
+                    </div>
                 }
             </div>
-            {imageUrl &&
-                <div>
-                    <AvatarEditor
-                        image={imageUrl}
-                        width={WIDTH}
-                        height={HEIGHT}
-                        border={[10,10]}
-                        borderRadius={0}
-                        color={[255,255,255,0.6]}
-                        scale={scale}
-                        rotate={0}
-                        ref={(e) => setEditor(e)}
-                    />
-                    <div>
-                        <label>Scale</label>
-                        <input
-                            type='range'
-                            step='0.1'
-                            min='1'
-                            max='3'
-                            value={scale}
-                            onChange={(e) => setScale(parseFloat(e.target.value))}
-                        />
-                    </div>
-                    <div>
-                        <label>Make Photo Private</label>
-                        <input
-                            type='checkbox'
-                            checked={privatePhoto}
-                            onChange={() => setPrivatePhoto(!privatePhoto)}
-                        />
-                    </div>
-                    <button disabled={imageUploaded} onClick={uploadPhoto}>Upload Photo</button>
-                </div>
-            }
-        </div>
+        </Modal>
     )
 }
 
