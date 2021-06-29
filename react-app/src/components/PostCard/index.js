5929152a3805b0001bffe68962f0cd06fb8d81dd
Chuck Kainrath <chuckkainrath@gmail.com>
2021-06-29 18:21:15 -0500

    added instructions for postcards

--- react-app/src/components/PostCard/index.js
+++ react-app/src/components/PostCard/index.js
@@ -28,9 +28,25 @@ function PostCard() {
 
 
     return (
-        <div className={styles.kanvas__container}>
-            <h1 className={styles.postcard_title}><span className={styles.info}><i class="fad fa-info-circle"></i></span>Create a Postcard</h1>
-            <PhotoKanvas stage={stage} setStage={setStage} photoSrc={imageUrl} />
+        <div className={stage !== 'complete' ? styles.kanvas__container : null}>
+            {stage === 'card' &&
+                <div className={styles.info}><i class="fad fa-info-circle"></i></div>
+            }
+            <div className={styles.instructions}>
+                <h1>Instructions</h1>
+                <ul>
+                    <li>Click 'New Text' to create a new text field</li>
+                    <li>An active text field is indicated by a red dotted border</li>
+                    <li>The active text field is editable and draggable</li>
+                    <li>You can create as many text fields as you'd like</li>
+                    <li>Click a text field to make it active</li>
+                    <li>You can delete an active text field by clicking 'Delete'</li>
+                </ul>
+            </div>
+            <h1 className={styles.postcard_title}>Create a Postcard</h1>
+            <div className={styles.kanvas}>
+                <PhotoKanvas stage={stage} setStage={setStage} photoSrc={imageUrl} />
+            </div>
         </div>
     )
 }
